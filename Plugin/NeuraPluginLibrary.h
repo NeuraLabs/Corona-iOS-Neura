3a1edbf28cb3b426d6e8812343698fbdab21fb58
Youval Vaknin <youval@theneura.com>
2018-04-03 14:33:38 +0300

    changed PluginLibrary class name to NeuraPluginLibrary - will allow easier integration with other plugins

--- Plugin/NeuraPluginLibrary.h
+++ Plugin/NeuraPluginLibrary.h
@@ -11,8 +11,8 @@
 #include <CoronaLua.h>
 #include <CoronaMacros.h>
 
-// This corresponds to the name of the library, e.g. [Lua] require "plugin.neura"
+// This corresponds to the name of the library, e.g. [Lua] require "plugin.library"
 // where the '.' is replaced with '_'
-CORONA_EXPORT int luaopen_plugin_neura( lua_State *L );
+CORONA_EXPORT int luaopen_plugin_library( lua_State *L );
 
 #endif // _PluginLibrary_H__
